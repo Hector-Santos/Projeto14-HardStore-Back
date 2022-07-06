4ce27f7482cb275489d61b302f4c6ee3d9218646
Hector-Santos <habs.404@gmail.com>
2022-07-06 19:18:26 -0300

    inicializar repositorio

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import router from './routes/router.js';
+//import router from './routes/router.js';
 import cors from 'cors';
 
 
 const server = express();
 server.use(cors())
 server.use(express.json());
-server.use(router);
+//server.use(router);
 
 
 server.listen(process.env.PORT, () => {
