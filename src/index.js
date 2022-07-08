ebaae6844f3d8d88ff1514b2aae0c8ec79e3c453
Hector-Santos <habs.404@gmail.com>
2022-07-07 21:54:08 -0300

    adicionado arquivo router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './routes/router.js';
+import router from './routers/router.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import {Pesquisa}  from './controllers/produtosController.js';
