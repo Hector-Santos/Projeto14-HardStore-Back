2286d96f7b04b23aafef9040644b0b888509af35
Hector-Santos <habs.404@gmail.com>
2022-07-07 21:59:10 -0300

    adicionado arquivo router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import authRouter from './authRouter.js';
-import userRouter from './userRouter.js.js';
-import walletRouter from './walletRouter.js.js';
 
 
 
 const router = express.Router();
 router.use(authRouter);
-router.use(userRouter);
-router.use(walletRouter);
+
 
 
 export default router;
