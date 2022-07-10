e5864e21885d18a7c12e018e0cfb1f63b146cbf3
KadsonLima <kadson-kaio@hotmail.com>
2022-07-10 13:03:42 -0300

    feat: attUpdateCart

--- src/routes/storeRouter.js
+++ src/routes/storeRouter.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import {searchProduct, getCart, updateCart} from '../controllers/produtosController.js';
-import { validarCart } from '../middlewares/validarCart.js';
+import {searchProduct, getCart, updateCart, postCompras, getProdutos, addCart} from '../controllers/produtosController.js';
 import { validarToken } from '../middlewares/validarToken.js';
 
 
 const storeRouter = express.Router();
 storeRouter.get("/search", searchProduct);
 storeRouter.get("/cart",validarToken, getCart);
+storeRouter.post("/cart",validarToken, addCart);
 storeRouter.put("/cart/",validarToken, updateCart);
+storeRouter.post("/compras",validarToken, postCompras);
+storeRouter.get("/produtos",validarToken, getProdutos);
 
 
 
